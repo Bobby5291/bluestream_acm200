6384edec158e3e83d55652d1f1b507ba1cb05535
Bobby5291 <134861230+Bobby5291@users.noreply.github.com>
2025-12-15 21:43:49 +0000

    Update const.py

--- custom_components/bluestream_acm200/const.py
+++ custom_components/bluestream_acm200/const.py
@@ -1,3 +1,5 @@
+y
+
 from __future__ import annotations
 
 DOMAIN = "bluestream_acm200"
@@ -10,9 +12,12 @@ CONF_NUM_OUTPUTS = "num_outputs"
 
 CONF_POLL_INTERVAL = "poll_interval"
 
+# NEW
+CONF_INPUT_NAMES = "input_names"
+
 DEFAULT_PORT = 23
 DEFAULT_NUM_INPUTS = 4
-DEFAULT_NUM_OUTPUTS = 8
+DEFAULT_NUM_OUTPUTS = 9
 DEFAULT_POLL_INTERVAL = 5  # seconds
 
 PLATFORMS = ["select", "media_player", "sensor"]
